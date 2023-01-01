4a6d3091d7da263fd949d6020d4f7575e827278d
Retropaint <darkglasses1122@gmail.com>
2023-01-02 02:57:31 +0800

    moved all filter table to new service
    This helps optimize the page by not having to create the filters on every home page load

---
+++ src/app/filter.service.ts
@@ -0,0 +1,237 @@
+import { Injectable } from '@angular/core';
+import { Filter, FilterCategory, GeneratedFilter } from './interfaces/operator-filter';
+import { DatabaseService } from './services/database.service';
+import { SharedService } from './services/shared.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+
+  createdFilters: boolean = false;
+
+  generatedFilters: GeneratedFilter[] = [];
+
+  selectedAll: string = '';
+
+  classes: FilterCategory = 
+  {
+    name: 'Classes',
+    imgUrl: "https://raw.githubusercontent.com/Aceship/Arknight-Images/main/classes/class_",
+    isFolded: false,
+    filters: [
+      {
+        name: 'Caster',
+        toggle: false,
+        iconId: 'caster',
+      },
+      {
+        name: 'Defender',
+        toggle: false,
+        iconId: 'defender'
+      },
+      {
+        name: 'Guard',
+        toggle: false,
+        iconId: 'guard'
+      },
+      {
+        toggle: false,
+        name: "Medic",
+        iconId: 'medic'
+      },
+      {
+        toggle: false,
+        name: "Sniper",
+        iconId: 'sniper'
+      },
+      {
+        toggle: false,
+        name: "Specialist",
+        iconId: 'specialist'
+      },
+      {
+        toggle: false,
+        name: "Supporter",
+        iconId: 'supporter'
+      },
+      {
+        toggle: false,
+        name: "Vanguard",
+        iconId: 'vanguard'
+      },
+    ]
+  }
+
+  branches: FilterCategory = {
+    name: 'Branches',
+    filters: [],
+    imgUrl: 'assets/subclass/sub_',
+    imgUrlSuffix: '_icon',
+    isFolded: false
+  };
+
+  groups: FilterCategory = {
+    name: 'Groups',
+    filters: [],
+    imgUrl: 'assets/factions/logo_',
+    isFolded: false,
+  };
+
+  artists: FilterCategory = {
+    name: 'Artists',
+    filters: [],
+    isFolded: false
+  };
+
+  filters = [this.classes, this.branches, this.groups, this.artists];
+
+  constructor(
+    private database: DatabaseService,
+    private sharedService: SharedService
+  ) { }
+
+  createCategoryFilters() {
+    // used to sort branches by class
+    let associatedClasses: {
+      branch: string;
+      class: string;
+    }[] = [];
+
+    this.database.operators.forEach(operator => {
+      this.checkFilter(this.artists.filters, operator.skins[0].artist, operator.originalBranch);
+      this.checkFilter(this.groups.filters, operator.group.name, operator.group.id);
+      this.checkBranchFilter(this.branches.filters, operator.branch, operator.originalBranch, operator.class, associatedClasses);
+    })
+
+    this.artists.filters.sort((a, b) => (a.name > b.name) ? -1 : 1); 
+    this.groups.filters.sort((a, b) => (a.name > b.name) ? -1 : 1);
+    this.branches.filters.sort((a, b) => (a.name > b.name) ? -1 : 1);
+    
+    this.branches.filters.sort((a, b) => {
+      const aClass = associatedClasses.find(element => element.branch == a.name).class;
+      const bClass = associatedClasses.find(element => element.branch == b.name).class;
+
+      return (aClass > bClass) ? 1 : -1;
+    })
+  }
+
+  toggleFilter() {
+    this.selectedAll = null;
+    this.generatedFilters = [];
+
+    this.refreshBranchHighlights();
+    this.refreshGeneratedFilters();
+
+    this.sharedService.operatorListRefreshSubscription.next(this.generatedFilters);
+  }
+
+  private checkFilter(filters: Filter[], property: string, iconLink: string) {
+    const exists = filters.find(p => p.name == property);
+    if(!exists) {
+      filters.push({
+        name: property,
+        toggle: false,
+        iconId: iconLink,
+        isHighlighted: true
+      })
+    }
+  }
+
+  private checkBranchFilter(filters: Filter[], branch: string, iconLink: string, opClass: string, associatedClasses: any) {
+    const exists = filters.find(p => p.name == branch);
+    if(!exists) {
+      filters.push({
+        name: branch,
+        toggle: false,
+        iconId: iconLink,
+        associatedClass: opClass,
+        isHighlighted: true
+      })
+
+      const branchClassExists = associatedClasses.find(element => element.branch == branch);
+      if(!branchClassExists) {
+        associatedClasses.push(
+          {
+            branch: branch,
+            class: opClass
+          }
+        )
+      }
+    }
+  }
+
+  private sortCategory(filterName: string) {
+    this.refreshBranchHighlights();
+
+    this.filters.forEach(category => {
+      category.filters.forEach(filter => {
+        filter.toggle = false;
+      })
+    })
+
+    if(this.selectedAll == filterName) {
+      this.selectedAll = '';
+      this.generatedFilters = [];
+    } else {
+      this.generatedFilters = [
+        {
+          categoryName: filterName,
+          toggles: []
+        }
+      ]
+  
+      this.selectedAll = filterName;
+    }
+
+    this.sharedService.operatorListRefreshSubscription.next(this.generatedFilters);
+  }
+
+  private refreshGeneratedFilters() {
+    let allClassesOff = true;
+
+    this.filters.forEach(category => {
+      category.filters.forEach(filter => {
+        if(!allClassesOff && category.name == 'Branches' && !this.branchHighlight(filter.name, filter.associatedClass)) {
+          return;
+        }
+
+        if(filter.toggle) {
+          if(category.name == 'Classes') {
+            allClassesOff = false;
+          }
+
+          const filterCategory = this.generatedFilters.find(generatedFilter => generatedFilter.categoryName == category.name)
+          if(filterCategory) {
+            filterCategory.toggles.push(filter.name)
+          } else {
+            this.generatedFilters.push(
+              {
+                categoryName: category.name,
+                toggles: [filter.name]
+              }
+            )
+          }
+        }
+      })
+    })
+  }
+
+  private branchHighlight(branch: string, assClass: string) {
+    if(!this.classes.filters.find(filter => filter.name == assClass).toggle) {
+      const branchFilter = this.branches.filters.find(filter => filter.name == branch);
+      branchFilter.toggle = false;
+      branchFilter.isHighlighted = false;
+      
+      return false;
+    }
+
+    return true;
+  }
+
+  private refreshBranchHighlights() {
+    this.branches.filters.forEach(branch => {
+      branch.isHighlighted = true;
+    })
+  }
+}
